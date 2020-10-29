991cf98b63f2cfc2247f61260dc13d584ee33f91
Manan Sadana <manansadana455@gmail.com>
2020-10-29 17:12:07 +0530

    component hierarchy changed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ClassComponent from "./Components/ClassComponent/ClassComponent";
 import FunctionalComponent from "./Components/FunctionalComponent/FunctionalComponent";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
+import LifeCycleMethods from "./Components/LifeCycleMethods/LifeCycleMethods";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import NameDetails from "./Components/ClassComponent/NameCard/NameDetails/NameDetails";
@@ -15,6 +16,7 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/lifecyclemethods" component={LifeCycleMethods} />
             <Route path="/class" exact component={ClassComponent} />
             <Route path="/functional" component={FunctionalComponent} />
             <Route path="/class/:id" component={NameDetails} />
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,8 @@
 import React from "react";
-import LifeCycleMethods from "../LifeCycleMethods/LifeCycleMethods";
-
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <LifeCycleMethods />
     </div>
   );
 };
