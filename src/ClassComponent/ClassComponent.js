aa052cfa61d488420a6a0f2be04a9e7377833141
vjolly <vaibhavjolly22@gmail.com>
2020-11-03 17:08:59 +0530

    functional-&-classComponent

---
+++ src/ClassComponent/ClassComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class ClassComponent extends Component {
+    name= "manan";
+  skills = ["JS, ", "React, ", "HTML, ", "CSS, ", "Node "]
+  concepts = {
+    language: "React.js",
+    topic: "JSX variables"
+  }
+  learning = () =>{
+    return "We will be learning more about react libraries!"
+  }
+    render() {
+        return (
+            <div>
+            <h1>Hi, my name is {this.name}</h1>
+            <h3>I know how to program in {this.skills}.</h3>
+            <p classname= "topics">and this is my first {this.concepts.language} project using {this.concepts.topic}</p>
+            <p>{this.learning()} </p>
+            </div>
+        )
+    }
+}
+
+export default ClassComponent
---
+++ src/FunctionalComponent/FunctionalComponent.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+function List() {
+    //Now we create a nested list
+    const people = [
+        { 
+          name: 'chris',
+          pets: [
+            { name: 'bella', type: 'dog' },
+            { name: 'cocoa', type: 'dog' }
+          ]
+        },
+        { 
+          name: 'nick',
+          pets: [
+            { name: 'hilo', type: 'cat' },
+            { name: 'polly', type: 'cat' }
+          ]
+        }
+      ];
+    
+      return (
+        <div className= "list1">
+          {people.map((person, index) => (
+            <div key={index}>
+              <h2>{person.name}'s Pets</h2>
+    
+              {/* loop over the pets */}
+              <div className ="list2">
+                {person.pets.map((pet, i) => (
+                  <p key={i}>
+                    {pet.type} named {pet.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+
+
+
+
+
+
+
+
+
+    //This is a single list that we used in earlier file   
+//     const people = [
+//         { id: 1, name: 'Chris' },
+//         { id: 2, name: 'Nick' }
+//       ];
+//     return (  
+//         <div className= "list2">
+//         {people.map(person => (
+//             <p key={person.id}>{person.name}</p>
+//           ))}
+//           </div>
+//     );
+//   }
+
+  export default List;
