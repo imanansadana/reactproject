3b10a4a8309c96dfdfaeae82c673996aa851cf8d
vjolly <vaibhavjolly22@gmail.com>
2020-11-04 12:23:23 +0530

    JSX-variables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import ClassComponent from "./Components/ClassComponent/ClassComponent";
-import FunctionalComponent from "./Components/FunctionalComponent/FunctionalComponent";
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ClassComponent></ClassComponent>
-        <FunctionalComponent></FunctionalComponent>
+        hello!czxcxzdc
       </div>
     );
   }
