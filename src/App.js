978b125a4e47989ac2b824ba25e9b53e74613ccb
vjolly <vaibhavjolly22@gmail.com>
2020-11-04 12:35:31 +0530

    css left

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { Component } from "react";
 import "./App.css"
 
 class App extends Component {
+  name= "manan";
+  skills = ["JS, ", "React, ", "HTML, ", "CSS, ", "Node "]
+  concepts = {
+    language: "React.js",
+    topic: "JSX variables"
+  }
+  learning = () =>{
+    return "We will be learning more about react libraries!"
+  }
   render() {
-    name= "manan";
-    skills = ["JS, ", "React, ", "HTML, ", "CSS, ", "Node "]
-    concepts = {
-      language: "React.js",
-      topic: "JSX variables"
-    }
-    learning = () =>{
-      return "We will be learning more about react libraries!"
-    }
+  
     return (
       <div className="App">
-
+      <h1>Hi, my name is {this.name}</h1>
+      <h3>I know how to program in {this.skills}.</h3>
+      <p classname= "topics">and this is my first {this.concepts.language} project using {this.concepts.topic}</p>
+      <p>{this.learning()} </p>
       </div>
     );
   }
