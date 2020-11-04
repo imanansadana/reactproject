986e6547b930297642fbb81e641f5f64ed171b08
vjolly <vaibhavjolly22@gmail.com>
2020-11-04 12:33:29 +0530

    var added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import "./App.css"
 
 class App extends Component {
   render() {
+    name= "manan";
+    skills = ["JS, ", "React, ", "HTML, ", "CSS, ", "Node "]
+    concepts = {
+      language: "React.js",
+      topic: "JSX variables"
+    }
+    learning = () =>{
+      return "We will be learning more about react libraries!"
+    }
     return (
       <div className="App">
 
